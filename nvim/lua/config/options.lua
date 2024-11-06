a09b9910be85722756c38189283d366b2bdb32e5
Sebastian Mena <smf-mena@hotmail.com>
2024-11-06 10:51:48 -0500

    Gruvbox-material theme added

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,5 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.g.lazygit_config = false -- Let LazyVim automatically configures lazygit
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-lualine/lualine.nvim",
+
+  config = function()
+    local function sf_status()
+      local target_org = require("sf").get_target_org()
+      local covered_percent = require("sf").covered_percent()
+      return target_org .. "(" .. covered_percent .. ")"
+    end
+
+    require("lualine").setup({
+      options = {
+        theme = "gruvbox-material",
+      },
+      sections = {
+        lualine_c = { "filename", sf_status },
+      },
+    })
+  end,
+}
